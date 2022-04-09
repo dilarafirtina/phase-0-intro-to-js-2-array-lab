22f829fd9bb1d840f2f62063ca97e602dd98fb7c
Fatoş Dilara Fırtına <dilara_firtina@hotmail.com>
2022-04-09 13:41:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+let cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+destructivelyAppendCat("Ralph");
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+destructivelyRemoveLastCat();
+destructivelyRemoveFirstCat();
+
+
+function appendCat(name){
+    let newCat = [...cats, name];
+    return newCat;
+}
+
+appendCat("Broom");
+
+function prependCat(name){
+    let newCat = [name, ...cats];
+    return newCat;
+}
+
+prependCat("Arnold");
+
+
+function removeLastCat(){
+    let newCat = cats.slice(0, cats.length-1);
+    return newCat;
+}
+
+function removeFirstCat(){
+    let newCat = cats.slice(1);
+    return newCat;
+}
